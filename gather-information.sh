46a5e2576350f4abad91023ee297e80de2932533
pevic sanch <sanchez.pv@pm.me>
2022-08-17 13:11:37 +0200

    adding scrip exemple

---
+++ gather-information.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+
+#this is a example script to run in the terminal
+# chmod +x <filename> to make ejecutale
+
+echo "Starting at: $(date)"
+echo
+
+echo "UPTIME"
+uptime
+echo
+
+echo "FREE"
+free
+echo
+
+
+echo "WHO"
+who
+echo
+
+echo "Finishing at: $(date)"
